ba051dc2e435fe95820ac17679af0695c1a734e4
jzybert <jmzybert@gmail.com>
2018-10-18 09:26:32 -0400

    Need port keyword

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ config :task_tracker, TaskTrackerWeb.Endpoint,
   server: true,
   root: ".",
   version: Application.spec(:phoenix_distillery, :vsn),
-  http: [:inet6, {:system, "PORT"}],
+  http: [:inet6, port: {:system, "PORT"}],
   url: [host: "tasks1.jonathanzybert.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
